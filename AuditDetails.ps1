f1920fa816d8ecc690954784c781c85bc1f598cb
jahube <70081790+jahube@users.noreply.github.com>
2021-03-20 00:20:42 +0200

    Update AuditDetails.ps1

--- AuditDetails.ps1
+++ AuditDetails.ps1
@@ -1,5 +1,4 @@
-#
-start-transcript -verbose
+﻿start-transcript -verbose
 
 $user = ""  # modify AFFECTED@USER.com
 
@@ -23,46 +22,39 @@ Try { Connect-ExchangeOnline -Credential $Credentials -EA stop } catch { Connect
 IF (!($Credentials.UserName -in (get-RoleGroupMember "Organization Management").primarySMTPaddress)) { Add-RoleGroupMember "Organization Management" -Member $ADMIN
 Try { Connect-ExchangeOnline -Credential $Credentials -EA stop } catch { Connect-ExchangeOnline -UserPrincipalName $ADMIN } }
 
-Try {$All = Get-ExoMailbox -ResultSize unlimited -EA stop } catch { $All = get-mailbox -ResultSize unlimited } 
+Try {$All = Get-ExoMailbox -ResultSize unlimited -Properties Name,PrimarySMTPaddress,Displayname,Userprincipalname,Guid,DistinguishedName,alias -EA stop } catch { $All = get-mailbox -ResultSize unlimited }
  IF ($All.Count -gt "400") { $Data = Read-Host -Prompt "Affected User [Userprincipalname]" }                      # Above threshold - ask for manual user input
-                      ELSE { $Data = @($All | select Pr*ess,Dis*me,Use*me | Out-GridView -Passthru -Title "Select User").userprincipalname }} # below threshold
+                      ELSE { $Data = @($All | select PrimarySMTPaddress,Displayname,Userprincipalname,alias,name | Out-GridView -Passthru -Title "Select User").userprincipalname } # below threshold
 
 # enable Unified Audit logs
 IF(!((Get-AdminAuditLogConfig).UnifiedAuditLogIngestionEnabled)) {
 Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true ;
 Write-host "Unified Audit log was disabled - ENABLING NOW" -F yellow }
 
-Foreach ($U in $Data) { Try { $M = Get-ExoMailbox $U -EA stop } catch { $M = get-mailbox $U }
+Foreach ($U in $Data) {
+ Try { $M = Get-ExoMailbox $U -Properties Name,PrimarySMTPaddress,Displayname,Userprincipalname,Guid,DistinguishedName -PropertySets Audit -EA stop } catch { $M = get-mailbox $U }
 
- Write-host "BEFORE: [AuditOwner] $(@($M.AuditOwner).count) [AuditOwner]  $($M.AuditOwner)" -F yellow
- Write-host "BEFORE: [ Delegate ] $(@($M.AuditDelegate).count) [ Delegate ]  $($M.AuditDelegate)" -F yellow
- Write-host "BEFORE: [AuditAdmin] $(@($M.AuditAdmin).count) [ Delegate ]  $($M.AuditAdmin)" -F yellow
+ Write-host "BEFORE: $(@($M.AuditOwner).count) [Owner]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditOwner)`n" -F Green
+ Write-host "BEFORE: $(@($M.AuditDelegate).count) [Delegate]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditDelegate)`n" -F cyan
+ Write-host "BEFORE: $(@($M.AuditAdmin).count) [Admin]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditAdmin)`n" -F White
 
-        $All = 'Create', 'HardDelete', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions','SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateInboxRules'
-$OWNRandDLGT =  'AddFolderPermissions', 'ApplyRecord', 'ModifyFolderPermissions','Move','UpdateCalendarDelegation' ;  $Owner = 'Send', 'MailboxLogin'
-   $Delegate = 'FolderBind', 'SendAs', 'SendOnBehalf' ;  $Admin = 'Copy', 'SendAs', 'SendOnBehalf', 'UpdateCalendarDelegation'
-
-                       # Apply ALL DETAILS
-         $Parameter = @{ identity = $user ;  
-                     AuditEnabled = $true ;
-                       AuditOwner = $All + $OWNRandDLGT + $Owner
-                    AuditDelegate = $All + $OWNRandDLGT + $Delegate
-                       AuditAdmin = $All + $Admin }
-                      Set-Mailbox @Parameter 
-
-                      Set-Mailbox -Identity $M.UserPrincipalname -AuditEnabled $false        #OFF
-                      Set-Mailbox -Identity $M.UserPrincipalname -AuditEnabled $true         #ON = update refresh Unified Audit
+$Param = @{ AuditOwner = 'AddFolderPermissions', 'ApplyRecord', 'Create', 'Send', 'HardDelete', 'MailboxLogin', 'ModifyFolderPermissions', 'Move', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateCalendarDelegation', 'UpdateInboxRules' ;
+         AuditDelegate = 'AddFolderPermissions', 'ApplyRecord', 'Create', 'FolderBind', 'HardDelete', 'ModifyFolderPermissions', 'Move', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SendAs', 'SendOnBehalf', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateInboxRules' ;
+            AuditAdmin = 'Copy', 'Create', 'HardDelete', 'MoveToDeletedItems', 'RecordDelete', 'RemoveFolderPermissions', 'SendAs', 'SendOnBehalf', 'SoftDelete', 'Update', 'UpdateFolderPermissions', 'UpdateCalendarDelegation', 'UpdateInboxRules' }
+           Set-Mailbox -identity $M.UserPrincipalName -AuditEnabled $true @Param ;  
+         
+                    # Set-Mailbox -Identity $M.UserPrincipalname -AuditEnabled $false        #OFF
+                    # Set-Mailbox -Identity $M.UserPrincipalname -AuditEnabled $true         #ON = update refresh Unified Audit
 set-MailboxAuditBypassAssociation -Identity $M.UserPrincipalname -AuditBypassEnabled $true   #OFF
 set-MailboxAuditBypassAssociation -Identity $M.UserPrincipalname -AuditBypassEnabled $false  #ON = update refresh Mbx Audit
-
- Try { $M = Get-ExoMailbox $U -EA stop } catch { $M = get-mailbox $U }
-
- Write-host "AFTER: [AuditOwner] $(@($M.AuditOwner).count) [AuditOwner]  $($M.AuditOwner)" -F green
- Write-host "AFTER: [ Delegate ] $(@($M.AuditDelegate).count) [ Delegate ]  $($M.AuditDelegate)" -F green
- Write-host "AFTER: [AuditAdmin] $(@($M.AuditAdmin).count) [ Delegate ]  $($M.AuditAdmin)" -F green
+   
+   Write-host $M.DisplayName ' ⋆⋅☆⋅⋆ Settings Applied  ⋆⋅☆⋅⋆ ' -F yellow -B DarkBlue
+ Try { $M = Get-ExoMailbox $U -Properties Name,PrimarySMTPaddress,Displayname,Userprincipalname,Guid,DistinguishedName -PropertySets Audit -EA stop } catch { $M = get-mailbox $U }
+ $M | fl *audit*
+ Write-host "AFTER: $(@($M.AuditOwner).count) [Owner]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditOwner)`n" -F Green
+ Write-host "AFTER: $(@($M.AuditDelegate).count) [Delegate]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditDelegate)`n" -F cyan
+ Write-host "AFTER: $(@($M.AuditAdmin).count) [Admin]" -F Yellow -n  ; Write-host "- Audited Operations: `n $($M.AuditAdmin)`n" -F White
 
  } # End Foreach per User
 
 stop-transcript
-
-#End Script
